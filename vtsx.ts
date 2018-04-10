1e024d2d6b8b654f5e94c592242db2af7c51b8af
hjboss <hongjiangproject@gmail.com>
2018-04-10 18:58:40 +0800

    Initial commit

---
+++ vtsx.ts
@@ -0,0 +1,12 @@
+// Copyright 2017 The HongJiang Library Authors. All right reserved.
+// Use of this source that is governed by a Apache-style
+// license that can be found in the LICENSE file.
+//
+// 支持Typescript和JSX的Vue腳手架
+//
+// @authors hjboss <hongjiangproject@gmail.com> 2018-04 $$
+import * as fs from 'fs'
+import * as path from 'path'
+
+(() => {
+})()
