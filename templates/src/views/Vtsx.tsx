a5b994f4a6329d1ae314f7f8f025584580df1d12
hjboss <hongjiangproject@gmail.com>
2018-04-14 17:38:03 +0800

    Initial commit

--- templates/src/views/Vtsx.tsx
+++ templates/src/views/Vtsx.tsx
@@ -3,7 +3,7 @@ import { Component, Prop } from 'vue-property-decorator'
 import { theme } from '../config'
 
 @Component
-export default class Draw extends Vue {
+export default class vtsx extends Vue {
         public render(h: CreateElement) {
                 return (
                         <div class={theme.prefix + '-viewport'}>Hello, vtsx ~ </div>
